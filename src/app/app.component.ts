02ce67957fdf43bcafa5d398e4d9d7da15220e44
fie2377 <fie2377@gmail.com>
2022-05-06 20:32:15 -0300

    conflito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,5 @@ export class AppComponent {
 
   contador = 0;
   title = 'ngrx-fie';
-<<<<<<< HEAD
-
-
-  teste2 = "joao2"
-=======
   teste = 'joao'
->>>>>>> 2faf40f2937df87e5fff18340e92fb704873304d
 }
